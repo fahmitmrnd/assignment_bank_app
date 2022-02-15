b125ab14511f0a0060b5f10b4956136c463bf16c
Pham Thanh Tam <pttam.110494@gmail.com>
2022-02-15 20:15:27 +0700

    created profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from "./pages/home/home.component";
 import { LoginComponent } from "./pages/login/login.component";
+import { ProfileComponent } from "./pages/profile/profile.component";
 import { SignupComponent } from "./pages/signup/signup.component";
 
 const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
+  },
   {
     path: 'login',
     component: LoginComponent
--- src/app/pages/page.module.ts
+++ src/app/pages/page.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from "@angular/core";
-import { SharedModule } from "../shared/shared.module";
-import { LoginComponent } from "./login/login.component";
-import { SignupComponent } from "./signup/signup.component";
+import { NgModule } from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
-  declarations: [LoginComponent, SignupComponent],
-  imports: [SharedModule]
+  declarations: [
+    HomeComponent,
+    LoginComponent,
+    SignupComponent,
+    ProfileComponent,
+  ],
+  imports: [SharedModule],
 })
 export class PageModule {}
